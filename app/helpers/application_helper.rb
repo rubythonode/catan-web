d8a7d76c0ca946ce349f4b4162dd3f9ff80fdb2e
dali <dali@ufofactory.org>
2016-02-19 17:38:18 +0900

    style articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,24 @@ module ApplicationHelper
   def issue_home_path(issue)
     slug_issue_path(slug: issue.title)
   end
+
+  def choice_icon(subject)
+    choice_value = subject
+    choice_value = subject.choice if subject.respond_to?(:choice)
+    return if choice_value.nil?
+    case choice_value.to_sym
+    when :agree
+      fa_icon('thumbs-up')
+    when :disagree
+      fa_icon('thumbs-down')
+    end
+  end
+
+  def date_f(date)
+    if date.today?
+      date.strftime("%H:%M")
+    else
+      date.strftime("%Y.%m.%d")
+    end
+  end
 end
