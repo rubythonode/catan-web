d4154fcc91e074ba8af4637ef24f6b0595d20430
dali <dali@ufofactory.org>
2016-02-21 16:45:15 +0900

    email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'canoe.parti.xyz' }
-  config.action_mailer.asset_host =  'http://canoe.parti.xyz'
+  config.action_mailer.default_url_options = { :host => 'parti.xyz' }
+  config.action_mailer.asset_host =  'http://parti.xyz'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
