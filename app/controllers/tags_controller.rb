57bd899faf3b62212a268b833c5351b4ca3d5bb4
mozodev <mozodev@users.noreply.github.com>
2016-02-22 20:01:52 +0900

    hotfix: add meta tags for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
   def show
+    prepare_meta_tags title: params[:name],
+                      description: params[:name]
     @posts = Post.recent.tagged_with(params[:name])
     @posts_for_filter = @posts
     if params[:t].present?
