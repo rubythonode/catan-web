735c47b26a41e3e248d30b11f764b8ca4913387d
dali <dali@ufofactory.org>
2016-02-27 23:19:45 +0900

    sort issues in user home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def home
     @user = User.find_by! nickname: params[:nickname].try(:downcase)
-    @posts = @user.posts.for_list
+    @posts = @user.posts.for_list.recent
     @posts_for_filter = @posts
     @posts = filter_posts(@posts)
     @postables = @posts.map &:postable
