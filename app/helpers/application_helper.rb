778ab0f6f99154c9d998e20e81938a260b709bae
dali <dali@ufofactory.org>
2016-03-05 15:47:42 +0900

    bugfix social card of opinion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,10 +17,22 @@ module ApplicationHelper
   end
 
   def asset_data_base64(path)
-    asset = Rails.application.assets.find_asset(path)
-    throw "Could not find asset '#{path}'" if asset.nil?
+    content, content_type = parse_asset(path)
+    base64 = Base64.encode64(content).gsub(/\s+/, "")
+    "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
+  end
 
-    base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
-    "data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
+  def parse_asset(path)
+    if Rails.application.assets
+      asset = Rails.application.assets.find_asset(path)
+      throw "Could not find asset '#{path}'" if asset.nil?
+      return asset.to_s, asset.content_type
+    else
+      name = Rails.application.assets_manifest.assets[path]
+      throw "Could not find asset '#{path}'" if name.nil?
+      content_type = MIME::Types.type_for(name).first.content_type
+      content = open(File.join(Rails.public_path, 'assets', name)).read
+      return content, content_type
+    end
   end
 end
