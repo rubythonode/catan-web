bf2ed68e1539f6f25e8bff44893b0db251783362
dali <dali@ufofactory.org>
2016-03-06 23:57:54 +0900

    symlink to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 SitemapGenerator::Sitemap.default_host = "http://parti.xyz"
-
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.create do
   Issue.find_each do |issue|
     add slug_issue_path(slug: issue.slug), changefreq: 'daily', lastmod: issue.posts.newest.try(:updated_at) || issue.updated_at
--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,3 @@
-%w(uploads google28d5b131c2b1660c.html).each do |folder|
+%w(uploads google28d5b131c2b1660c.html sitemaps).each do |folder|
     run "ln -nfs #{config.shared_path}/public/#{folder} #{config.release_path}/public/#{folder}"
 end
