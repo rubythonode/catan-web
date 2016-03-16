a41d24a8e12d6c906496ce56391635e656949bb6
mozodev <mozodev@users.noreply.github.com>
2016-03-16 19:36:23 +0900

    allow update without password for social accounts

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   include AfterLogin
   after_filter :after_omniauth_login, only: :create
 
+  # Overwrite update_resource to let users to update their user without giving their password
+  def update_resource(resource, params)
+    if current_user.provider == "facebook"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
+
   private
 
   def sign_up_params
