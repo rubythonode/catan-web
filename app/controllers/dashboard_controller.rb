696fee1dbab25db14c66f8dd2b641cca733af027
mozodev <mozodev@users.noreply.github.com>
2016-03-16 16:19:36 +0900

    add intro page for new users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,11 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @issues = Issue.all
+    @issue = Issue::OF_ALL
+    @watched_issues = current_user.watched_issues
+    render 'intro' and return if need_to_watch
+
     watched_posts = Post.for_list.recent.watched_by(current_user)
     @watched_posts_for_filter = watched_posts
     @past_day_postables = @watched_posts_for_filter.past_day.map &:postable
@@ -17,5 +22,11 @@ class DashboardController < ApplicationController
   def campaign
     @posts = current_user.watched_posts.only_opinions.recent.page params[:page]
     @opinions = @posts.all.map &:postable
+  end
+
+  private
+
+  def need_to_watch
+    current_user.watched_issues.size < 2
   end
 end
